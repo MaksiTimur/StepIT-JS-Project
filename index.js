e8a7e49b67b37f085e6e7e9bc0502be84e8856ed
Timur <144324698+MaksiTimur@users.noreply.github.com>
2023-11-03 11:29:48 +0400

    Генерация id

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ class Task {
     #description;
     #cost;
 
-    constructor (id, description, cost) {
+    constructor (description, cost) {
         if (new.target) throw new Error('Cannot be invoked with "new"')
 
-        this.#id = id;
+        this.#id = function() {
+            return Date.now().toString(36) + Math.random().toString(36).substr(2);
+        };
         this.#description = description;
         this.#cost = Number(cost);
     }
