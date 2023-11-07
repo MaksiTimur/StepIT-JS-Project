284a86bb8a809225d081da3b8c11ebce80eb12ba
Timur <144324698+MaksiTimur@users.noreply.github.com>
2023-11-07 19:11:00 +0400

    Доработка класса Task

--- index.js
+++ index.js
@@ -3,38 +3,38 @@ console.clear();
 
 function Task(description, cost) {
 
-    if (new.target) throw new Error('Forbidden to create an instance of this class');
-    if (!new.target) throw new Error('Forbidden to create an instance of this class');
-
-    const generateID = function() {
-        return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    if (new.target === Task) {
+        throw new Error('Task is an abstract class and cannot be instantiated directly');
     }
+    if (cost < 0) throw new Error('Cost cannot be less than zero');
 
-    const _id = generateID(); // Каждый раз создаёт новый id
+    const _id = Date.now().toString(36) + Math.random().toString(36).substr(2);
     const _description = description;
     const _cost = Number(cost);
 
-    Object.defineProperty(this, 'id', {
-        get() {
-            return _id;
-        }
-    });
+    Object.defineProperties(this, {
+        id: {
+            get() {
+                return _id;
+            }
+        },
 
-    Object.defineProperty(this, 'description', {
-        get() {
-            return _description;
-        }
-    });
+        description: {
+            get() {
+                return _description;
+            }
+        },
 
-    Object.defineProperty(this, 'cost', {
-        get() {
-            return _cost;
+        cost: {
+            get() {
+                return _cost;
+            }
         }
     });
 }
 
 class IncomeTask extends Task {
-    constructor (description, cost) {
+    constructor(description, cost) {
         super(description, cost);
     }
 
@@ -48,8 +48,8 @@ class IncomeTask extends Task {
 }
 
 class ExpenseTask extends Task {
-    constructor () {
-
+    constructor(description, cost) {
+        super(description, cost);
     }
 
     makeDone(budget) {
@@ -59,40 +59,4 @@ class ExpenseTask extends Task {
     makeUnDone(budget) {
         budget.expense -= this.cost;
     }
-}
-
-class TasksController {
-    #tasks = [];
-
-    constructor () {
-        // Конструктор
-    }
-
-    addTasks(...tasks) {
-        tasks.push(...tasks);
-    }
-    
-    deleteTask(task) {
-        const indexOfTask = _footbalPlayers.indexOf(task);
-
-        if (indexOfTask !== -1) {
-            tasks.splice(indexOfTask, 1);
-        }  
-    }
-
-    getTasks() {
-        return [...tasks]; 
-    }
-
-    getTasksSortedBy(filterType) {
-        const filteredTasks = [];
-
-
-    }
-
-    getFilteredTasks() {
-        
-    }
-
-    // Должен иметь механизм работы с уже сделанными задачами (BudgetController)
 }
